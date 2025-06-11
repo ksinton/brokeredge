32b41eb274cdd046530247adfeaa476963013962
kimsinton <kimsinton@gmail.com>
2025-06-11 15:55:14 -0500

    also trying to fix  host issues that happened with production build by stripping www from the host in the middleware

--- broker-edge-frontend/src/middleware.ts
+++ broker-edge-frontend/src/middleware.ts
@@ -1,9 +1,13 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
 export function middleware(request: NextRequest) {
-    const host = request.headers.get('host') || ''
-    const response = NextResponse.next()
-    response.headers.set('x-custom-host', host)
-    return response
+    let host = request.headers.get('host') || '';
+
+    // Remove leading "www." if present
+    host = host.replace(/^www\./i, '');
+
+    const response = NextResponse.next();
+    response.headers.set('x-custom-host', host);
+
+    return response;
 }
